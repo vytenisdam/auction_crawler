9bfd6d8a23e3904b0f53902478c79bc786b5b221
Vytenis Dambrauskas <vytenisdambrauskas@gmail.com>
2023-12-21 16:33:21 +0200

    Added functionality for selenium crawl to scroll infinitely or for estimated time

--- auction_crawler/selenium_crawl.py
+++ auction_crawler/selenium_crawl.py
@@ -1,25 +1,54 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.wait import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
 
-webdriver_path = 'C:/Users/Vytenis/PycharmProjects/crawler_project/crawler_project/chromedriver-win64/chromedriver-win64'
+import time
+
 
 driver = webdriver.Chrome()
+driver.set_window_size(1500, 1400)
 
 url = 'https://foros.lt/auctions/'
-# url = 'https://quotes.toscrape.com/'
 
 driver.get(url)
 checkbox = driver.find_element(By.XPATH, '//input[contains(@type, "checkbox")]')
+#takes a screenshot before unchecking
+# driver.save_screenshot('1.png')
 if checkbox.is_selected():
     checkbox.click()
-    driver.implicitly_wait(10)
-    for i in driver.find_elements(By.XPATH, "//p[contains(@class, 'md:mb-auto')]"):
-        print(i.text)
-    # data_elements = driver.find_elements(By.XPATH, "//p[contains(@class, 'md:mb-auto')]")
-# data_elements = driver.find_elements(By.XPATH, "//span[@class = 'text']")
-#     for i in data_elements:
-#         print(i.text)
+# Should be a better option
+time.sleep(2)
 
-driver.quit()
+SCROLL_PAUSE_TIME = 2
+
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+time_to_break = -2
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+    time_to_break += 2
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        print('done crawling end of the page')
+        break
+    elif time_to_break >= 20:
+        print(f'Done crawling for {time_to_break} seconds')
+        break
+    last_height = new_height
+
+# Cant make it to work
+# driver.implicitly_wait(50)
+#takes a screenshot after unchecking
+# driver.save_screenshot('2.png')
+data_elements = driver.find_elements(By.XPATH, "//p[contains(@class, 'md:mb-auto')]")
+sum = 0
+for element in data_elements:
+    sum += 1
+    print(element.text)
+print(sum)
+
+
