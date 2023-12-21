69fb811879bc88fb86ab43d73d5a93a4d112ad96
Vytenis Dambrauskas <vytenisdambrauskas@gmail.com>
2023-12-21 15:23:39 +0200

    Added csv write and read functionality

---
+++ auction_crawler/csv_write.py
@@ -0,0 +1,10 @@
+import csv
+
+
+def file_write(data_to_write):
+    """Writes a crawled data to a csv file using a DictWriter method."""
+    with open('crawl_data.csv', 'w', encoding='utf-8', newline='') as file:
+        fieldnames = ['Property name', 'Property price', 'Auction end date', 'Auction link']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data_to_write)
